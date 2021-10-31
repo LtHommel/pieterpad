a65a1ebdf764c50d46f049c906eba44fa91aed59
Wilhelmina <willemijnsprangers@protonmail.com>
2021-10-31 18:49:36 +0100

    toevoegen fotos

---
+++ assets/imgs/photos/photos.js
@@ -0,0 +1,300 @@
+const photos = [{
+  url: "./assets/imgs/photos/IMG_2561.jpg",
+  lat: 53.0033638888889,
+  long: 6.65141944444444
+},
+{
+  url: "./assets/imgs/photos/IMG_2549.jpg",
+  lat: 53.0697315277778,
+  long: 6.65794230555556
+},
+{
+  url: "./assets/imgs/photos/IMG_2548.jpg",
+  lat: 53.0697315277778,
+  long: 6.65794230555556
+},
+{
+  url: "./assets/imgs/photos/IMG_2560.jpg",
+  lat: 53.0033611111111,
+  long: 6.6514
+},
+{
+  url: "./assets/imgs/photos/IMG_2558.jpg",
+  lat: 53.0047388888889,
+  long: 6.65170833333333
+},
+{
+  url: "./assets/imgs/photos/IMG_2559.jpg",
+  lat: 53.00335,
+  long: 6.65143333333333
+},
+{
+  url: "./assets/imgs/photos/IMG_2538.jpg",
+  lat: 53.0743611111111,
+  long: 6.66106111111111
+},
+{
+  url: "./assets/imgs/photos/IMG_2539.jpg",
+  lat: 53.0742666666667,
+  long: 6.66095833333333
+},
+{
+  url: "./assets/imgs/photos/IMG_2537.jpg",
+  lat: 53.1765553611111,
+  long: 6.57866336111111
+},
+{
+  url: "./assets/imgs/photos/IMG_2523.jpg",
+  lat: 53.3447527777778,
+  long: 6.48403055555556
+},
+{
+  url: "./assets/imgs/photos/IMG_2522.jpg",
+  lat: 53.4000083333333,
+  long: 6.45330833333333
+},
+{
+  url: "./assets/imgs/photos/IMG_2536.jpg",
+  lat: 53.1765555555556,
+  long: 6.57866388888889
+},
+{
+  url: "./assets/imgs/photos/IMG_2534.jpg",
+  lat: 53.1767166666667,
+  long: 6.57873888888889
+},
+{
+  url: "./assets/imgs/photos/IMG_2535.jpg",
+  lat: 53.1765555555556,
+  long: 6.57866388888889
+},
+{
+  url: "./assets/imgs/photos/IMG_2521.jpg",
+  lat: 53.4000277777778,
+  long: 6.45328055555556
+},
+{
+  url: "./assets/imgs/photos/IMG_2525.jpg",
+  lat: 53.3448527777778,
+  long: 6.48388611111111
+},
+{
+  url: "./assets/imgs/photos/IMG_2531.jpg",
+  lat: 53.2033055555556,
+  long: 6.56300833333333
+},
+{
+  url: "./assets/imgs/photos/IMG_2530.jpg",
+  lat: 53.2033166666667,
+  long: 6.56303888888889
+},
+{
+  url: "./assets/imgs/photos/IMG_2524.jpg",
+  lat: 53.3448555555556,
+  long: 6.48388055555556
+},
+{
+  url: "./assets/imgs/photos/IMG_2532.jpg",
+  lat: 53.1825,
+  long: 6.57096388888889
+},
+{
+  url: "./assets/imgs/photos/IMG_2533.jpg",
+  lat: 53.1824305555556,
+  long: 6.57118611111111
+},
+{
+  url: "./assets/imgs/photos/IMG_2554.jpg",
+  lat: 53.0172305555556,
+  long: 6.64925555555556
+},
+{
+  url: "./assets/imgs/photos/IMG_2540.jpg",
+  lat: 53.0742666666667,
+  long: 6.66095833333333
+},
+{
+  url: "./assets/imgs/photos/IMG_2555.jpg",
+  lat: 53.0172305555556,
+  long: 6.64925555555556
+},
+{
+  url: "./assets/imgs/photos/IMG_2557.jpg",
+  lat: 53.0047388888889,
+  long: 6.65170833333333
+},
+{
+  url: "./assets/imgs/photos/IMG_2556.jpg",
+  lat: 53.0047388888889,
+  long: 6.65170833333333
+},
+{
+  url: "./assets/imgs/photos/IMG_2546.jpg",
+  lat: 53.0697315277778,
+  long: 6.65794230555556
+},
+{
+  url: "./assets/imgs/photos/IMG_2552.jpg",
+  lat: 53.0697305555556,
+  long: 6.65794166666667
+},
+{
+  url: "./assets/imgs/photos/IMG_2553.jpg",
+  lat: 53.0172388888889,
+  long: 6.64925555555556
+},
+{
+  url: "./assets/imgs/photos/IMG_2547.jpg",
+  lat: 53.0697305555556,
+  long: 6.65794166666667
+},
+{
+  url: "./assets/imgs/photos/IMG_2551.jpg",
+  lat: 53.0697305555556,
+  long: 6.65794166666667
+},
+{
+  url: "./assets/imgs/photos/IMG_2550.jpg",
+  lat: 53.0697305555556,
+  long: 6.65794166666667
+},
+{
+  url: "./assets/imgs/photos/IMG_1726.jpg",
+  lat: 53.2462416666667,
+  long: 6.53718888888889
+},
+{
+  url: "./assets/imgs/photos/IMG_1719.jpg",
+  lat: 53.3264194444444,
+  long: 6.49328055555556
+},
+{
+  url: "./assets/imgs/photos/IMG_1725.jpg",
+  lat: 53.2552083333333,
+  long: 6.51342777777778
+},
+{
+  url: "./assets/imgs/photos/IMG_1724.jpg",
+  lat: 53.2584444444444,
+  long: 6.50546944444444
+},
+{
+  url: "./assets/imgs/photos/IMG_1718.jpg",
+  lat: 53.333325,
+  long: 6.51076944444444
+},
+{
+  url: "./assets/imgs/photos/IMG_1720.jpg",
+  lat: 53.3056333333333,
+  long: 6.49467777777778
+},
+{
+  url: "./assets/imgs/photos/IMG_1722.jpg",
+  lat: 53.3056055555556,
+  long: 6.49459166666667
+},
+{
+  url: "./assets/imgs/photos/IMG_1747.jpg",
+  lat: 53.1721944444444,
+  long: 6.58562222222222
+},
+{
+  url: "./assets/imgs/photos/IMG_1753.jpg",
+  lat: 53.0932722222222,
+  long: 6.684775
+},
+{
+  url: "./assets/imgs/photos/output.json",
+  lat: 53.0002472222222,
+  long: 6.64908888888889
+},
+{
+  url: "./assets/imgs/photos/IMG_1743.jpg",
+  lat: 53.1757361111111,
+  long: 6.57837777777778
+},
+{
+  url: "./assets/imgs/photos/IMG_1781.jpg",
+  lat: 53.0696916666667,
+  long: 6.65706111111111
+},
+{
+  url: "./assets/imgs/photos/IMG_1795.jpg",
+  lat: 53.0173083333333,
+  long: 6.64919166666667
+},
+{
+  url: "./assets/imgs/photos/IMG_1754.jpg",
+  lat: 53.0933333333333,
+  long: 6.68481111111111
+},
+{
+  url: "./assets/imgs/photos/IMG_1741.jpg",
+  lat: 53.1826361111111,
+  long: 6.57380277777778
+},
+{
+  url: "./assets/imgs/photos/IMG_1782.jpg",
+  lat: 53.0424166666667,
+  long: 6.65793888888889
+},
+{
+  url: "./assets/imgs/photos/IMG_1764.jpg",
+  lat: 53.0743833333333,
+  long: 6.66005555555556
+},
+{
+  url: "./assets/imgs/photos/IMG_1773.jpg",
+  lat: 53.0696305555556,
+  long: 6.65030555555556
+},
+{
+  url: "./assets/imgs/photos/IMG_1766.jpg",
+  lat: 53.0739361111111,
+  long: 6.657775
+},
+{
+  url: "./assets/imgs/photos/IMG_1799.jpg",
+  lat: 53.0002472222222,
+  long: 6.64908888888889
+},
+{
+  url: "./assets/imgs/photos/IMG_1789.jpg",
+  lat: 53.0424166666667,
+  long: 6.65793888888889
+},
+{
+  url: "./assets/imgs/photos/IMG_1777.jpg",
+  lat: 53.0699111111111,
+  long: 6.65535833333333
+},
+{
+  url: "./assets/imgs/photos/IMG_1749.jpg",
+  lat: 53.1625416666667,
+  long: 6.60313611111111
+},
+{
+  url: "./assets/imgs/photos/IMG_1760.jpg",
+  lat: 53.0932694444444,
+  long: 6.68473888888889
+},
+{
+  url: "./assets/imgs/photos/IMG_1738.jpg",
+  lat: 53.1835833333333,
+  long: 6.56833611111111
+},
+{
+  url: "./assets/imgs/photos/IMG_1739.jpg",
+  lat: 53.1835638888889,
+  long: 6.56838888888889
+},
+{
+  url: "./assets/imgs/photos/IMG_1729.jpg",
+  lat: 53.218675,
+  long: 6.561675
+},
+{
+  url: "./assets/imgs/photos/IMG_1716.jpg",
+  lat: 53.4000138888889,
+  long: 6.45319166666667
+}]
--- js/script.js
+++ js/script.js
@@ -6,74 +6,48 @@ L.tileLayer.provider('Stamen.Terrain').addTo(map);
 // ------ gpx ------
 
 const gpxs = [
-    'assets/gpx/etappes/01_pieterburen_winsum.gpx',
-    'assets/gpx/etappes/02_winsum_groningen_incl_omleiding.gpx',
-    'assets/gpx/etappes/03_groningen_zuidlaren.gpx',
-    'assets/gpx/etappes/04_zuidlaren_rolde.gpx',
-    'assets/gpx/etappes/05_rolde_schoonloo.gpx',
-    'assets/gpx/etappes/06_schoonloo_sleen.gpx',
-    'assets/gpx/etappes/07_sleen_coevorden.gpx',
-    'assets/gpx/etappes/08_coevorden_hardenberg.gpx',
-    'assets/gpx/etappes/09_hardenberg_ommen.gpx',
-    'assets/gpx/etappes/10_ommen_hellendoorn.gpx',
-    'assets/gpx/etappes/11_hellendoorn_holten.gpx',
-    'assets/gpx/etappes/12_holten_laren.gpx',
-    'assets/gpx/etappes/13_laren_vorden.gpx',
-    'assets/gpx/etappes/14_vorden_zelhem.gpx',
-    'assets/gpx/etappes/15_zelhem_braamt.gpx',
-    'assets/gpx/etappes/16_braamt_millingen.gpx',
-    'assets/gpx/etappes/17_millingen_groesbeek.gpx',
-    'assets/gpx/etappes/18_groesbeek_gennep.gpx',
-    'assets/gpx/etappes/19_gennep_vierlingsbeek.gpx',
-    'assets/gpx/etappes/20_vierlingsbeek_swolgen.gpx',
-    'assets/gpx/etappes/21_swolgen_venlo.gpx',
-    'assets/gpx/etappes/22_venlo_swalmen.gpx',
-    'assets/gpx/etappes/23_swalmen_montfort.gpx',
-    'assets/gpx/etappes/24_montfort_sittard.gpx',
-    'assets/gpx/etappes/25_sittard_strabeek.gpx',
-    'assets/gpx/etappes/26_strabeek_maastricht.gpx'
+    'assets/gpx/track/01_pieterburen_groningen.gpx',
+    'assets/gpx/track/02_groningen_zeegse.gpx',
+    'assets/gpx/track/03_zeegse_rolde.gpx'
 ]
-
-// gpxs.forEach(gpx => {
-//     new L.GPX(gpx, {
-//         async: true,
-//         marker_options: {
-//             startIconUrl: false,
-//             endIconUrl: false,
-//             shadowUrl: false
-//         }
-//     // }).on('loaded', function (e) {
-//     //     map.fitBounds(e.target.getBounds());
-//     }).addTo(map);
-// });
 
 new L.GPX('assets/gpx/pieterpad.gpx', {
     async: true,
     marker_options: {
         startIconUrl: false,
-        endIconUrl: 'assets/imgs/icons8-signpost-48.png',
-        wptIconUrls: {'': 'assets/imgs/icons8-signpost-48.png'},
+        endIconUrl: false,
         shadowUrl: false
+    },
+    polyline_options: {
+      color: 'blue',
+      opacity: 0.75,
+      weight: 3,
+      lineCap: 'round'
     }
 }).on('loaded', function (e) {
     console.log('loaded: ', e);
     map.fitBounds(e.target.getBounds());
 }).addTo(map);
 
-new L.GPX('assets/gpx/snurk.gpx', {
-  async: true,
-  marker_options: {
+gpxs.forEach((gpx, i) => {
+  var color = i % 2 === 0 ? 'red':'orange';
+
+  new L.GPX(gpx, {
+    async: true,
+    marker_options: {
       startIconUrl: false,
       endIconUrl: false,
-      wptIconUrls: {'': 'assets/imgs/icons8-sleeping-in-bed-48.png'},
-      shadowUrl: false
-  }
-}).on('loaded', function (e) {
-  console.log('loaded: ', e);
-  map.fitBounds(e.target.getBounds());
-}).addTo(map);
-
-
+      shadowUrl: false,
+    },
+    polyline_options: {
+      color: color,
+      opacity: 1,
+      weight: 2,
+      lineCap: 'round',
+      dashArray: '5,5'
+    }
+  }).addTo(map);
+});
 
 // ----- IMGs -----
 
@@ -88,14 +62,9 @@ var photoIcon = L.icon({
   popupAnchor:  [0, -4] // point from which the popup should open relative to the iconAnchor
 });
 
-L.marker([53.393719, 6.359244], {icon: photoIcon})
-    .addTo(map)
-    .bindPopup("<img class=\"popup\" src=\"assets/imgs/IMG_1152.JPG\"/>");
 
-L.marker([53.290192, 6.366372], {icon: photoIcon})
+photos.forEach(photo  => {
+L.marker([photo.lat, photo.long], {icon: photoIcon})
     .addTo(map)
-    .bindPopup("<img class=\"popup\" src=\"assets/imgs/IMG_1145.JPG\"/>");
-
-L.marker([53.398758, 6.454925], {icon: photoIcon})
-    .addTo(map)
-    .bindPopup("<img class=\"popup\" src=\"assets/imgs/IMG_1174.JPG\"/>");
+    .bindPopup(`<img class=\"popup\" src=${photo.url} />`);
+});
